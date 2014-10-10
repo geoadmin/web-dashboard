d12656d88df35539906ec9f446ba4ede0a7e8f71
ponceta <arnaud.poncet@swisstopo.ch>
2014-10-10 08:53:07 +0200

    webdashboard makeitrun

---
+++ jobs/twitter.job.js
@@ -0,0 +1,17 @@
+var twitter = require('twitter')
+// to be update to get #swisstopo tweets 
+/*client = Twitter::REST::Client.new do |config|
+config.consumer_key = 'YOUR_CONSUMER_KEY'
+config.consumer_secret = 'YOUR_CONSUMER_SECRET'
+end
+  
+  SCHEDULER.every '10m', :first_in => 0 do |job|
+  begin
+  client = twitter.search("$AAPL", :result_type => "recent").each.map do |tweet|
+  { name: tweet.text, body: tweet.user.screenname }
+  end
+  send_event('twitter_mentions', comments: tweets)
+  rescue
+  end
+  end
+*/
